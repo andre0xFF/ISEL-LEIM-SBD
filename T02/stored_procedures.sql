2d4bae75df7fadb605e56637b707484c5d0bf4db
Andre Fonseca <afbfonseca@gmail.com>
2017-01-29 15:00:55 +0000

    Updated SP

--- T02/stored_procedures.sql
+++ T02/stored_procedures.sql
@@ -1,14 +1,25 @@
-USE SBD;
+USE `sbd`;
+DROP procedure IF EXISTS `get_owner`;
 
-CREATE PROCEDURE `get_owner` (IN in_restaurant_id int)
+DELIMITER $$
+USE `sbd`$$
+CREATE PROCEDURE `get_owner` ()
 BEGIN
 	SELECT id, full_name, email, mobile_number FROM employee WHERE restaurant_id = in_restaurant_id AND owner = true;
-END
+END$$
 
-CREATE PROCEDURE `get_ready_orders`(IN in_date date, IN in_order_state varchar(20))
+DELIMITER ;
+
+DROP procedure IF EXISTS `get_ready_orders`;
+
+DELIMITER $$
+USE `sbd`$$
+CREATE PROCEDURE `get_ready_orders` ()
 BEGIN
 	SELECT client_order_id, MAX(order_state_id), employee_id, date FROM order_processing
 	WHERE date = in_date
 	GROUP BY client_order_id
 	HAVING MAX(order_state_id) = (SELECT id FROM order_state WHERE title = in_order_state);
-END
+END$$
+
+DELIMITER ;
